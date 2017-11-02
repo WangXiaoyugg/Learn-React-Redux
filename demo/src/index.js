03279e2764dceec93487648ce303b6527ec905aa
GarenWang <2270099149@qq.com>
2017-11-02 09:03:54 +0800

    redux 基础使用

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//新建store
+//通过reducer建立
+//根据老的state 和 action 生成新的state
+
+function counter(state=0,action){
+	switch (action.type) {
+		case '加机关枪':
+			return state+1
+		
+		case '减机关枪':
+			return state-1
+
+		default:
+			return 10
+			break;
+	}
+}
+
+const store = createStore(counter);
+
+const init = store.getState();
+console.log(init);
+
+function listener(){
+	const current = store.getState()
+	console.log(`现有机枪${current}吧`)
+}
+
+store.subscribe(listener);
+
+//派发事件
+store.dispatch({type:'加机关枪'});
+store.dispatch({type:"加机关枪"});
+store.dispatch({type:"减机关枪"});
