f17abf5fd24d156851476f51cb0e581989de3a25
GarenWang <2270099149@qq.com>
2017-11-02 21:59:20 +0800

    react-router 基本使用

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom'
 import { createStore,applyMiddleware, compose } from 'redux';
 import thunk  from 'redux-thunk'
 import { Provider } from 'react-redux';
+import { BrowserRouter,Route,Link } from 'react-router-dom'
+
 
 import App from './app'
 import { counter } from './index.redux'
@@ -13,9 +15,36 @@ const store = createStore(counter, compose(
 	window.devToolsExtension ? window.devToolsExtension(): f => f	
 ))
 
+function Erying(){
+	return <h2>二营</h2>
+}
+
+function Paobinglian(){
+	return <h2>炮兵连</h2>
+}
+
+
 ReactDom.render(
 		(<Provider store={store}>
-			<App/>
+			<BrowserRouter>
+				<div>
+					<ul>
+						<li>
+							<Link to='/'>一营</Link>
+						</li>
+						<li>
+							<Link to='/erying'>二营</Link>
+						</li>
+						<li>
+							<Link to='/paobinglian'>炮兵连</Link>
+						</li>
+						<Route path='/' exact component={App}></Route>
+						<Route path='/erying' component={Erying}></Route>
+						<Route path='/paobinglian' component={Paobinglian}></Route>
+					</ul>
+				</div>
+			</BrowserRouter>
 		</Provider>),
 		document.getElementById('root')
 );
+
