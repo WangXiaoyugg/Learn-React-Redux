38c77970eb9c39a5a43b67ae3b5c0a3286d6237e
GarenWang <2270099149@qq.com>
2017-11-02 20:54:40 +0800

    使用redux扩展插件

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { createStore,applyMiddleware } from 'redux';
+import { createStore,applyMiddleware, compose } from 'redux';
 import thunk  from 'redux-thunk'
 
 import App from './app'
 import { counter,addGUN,removeGUN,addGUNAsync } from './index.redux'
 
-const store = createStore(counter, applyMiddleware(thunk))
 
-
-
+const store = createStore(counter, compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension ? window.devToolsExtension(): f => f	
+))
 
 function render(){
 	ReactDom.render(<App store={store} addGUN={addGUN}  addGUNAsync={addGUNAsync} removeGUN={removeGUN} />,
