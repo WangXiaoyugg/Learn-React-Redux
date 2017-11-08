220873f15b68d41e1085ae8558a98eb4a6a31f5e
GarenWang <2270099149@qq.com>
2017-11-08 14:01:59 +0800

    数据库模型建立

--- imooc-app/server/model.js
+++ imooc-app/server/model.js
@@ -1,6 +1,36 @@
+const mongoose = require('mongoose')
 //链接mongo,并且使用imooc 这个集合
-const DB_URL = 'mongodb://127.0.0.1:27017/imooc'
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc-chat'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected',() => {
 	console.log('mongo connected success')
 })
+
+const models = {
+	user:{
+		'user':{type:String,require:true},
+		'pwd':{type:String,require:true},
+		'type':{type:String,require:true},
+		'avatar':{type:String},
+		//个人简介活职位简介
+		'desc':{type:String},
+		//职位名
+		'title':{type:String},
+		//如果是boss,还有两个字段
+		'company':{type:String},
+		'money':{type:String}
+	},
+	chat:{
+
+	}
+}
+
+for(let m in models){
+	mongoose.model(m,new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+	getModel:function (name) {
+		return mongoose.model(name);
+	}
+}
--- imooc-app/server/user.js
+++ imooc-app/server/user.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const model = require('./model.js')
+const User = model.getModel('user')
 const Router = express.Router()
 
-Router.get('/info',function (req,res) {
 
+Router.get('/list',function(req,res){
+	User.find({},function(err,doc){
+		if(err) throw new Error(err)
+		return res.json(doc)	
+	})
+})
+
+Router.get('/info',function (req,res) {
 	//验证用户的cookie
 	return res.json({code:1})
 })
