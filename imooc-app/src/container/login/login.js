a8958d529da5d63de986106232b672c25fa17c2a
GarenWang <2270099149@qq.com>
2017-11-12 15:32:10 +0800

    高阶组件imoocform重构注册登录页面

--- imooc-app/src/container/login/login.js
+++ imooc-app/src/container/login/login.js
@@ -4,47 +4,8 @@ import {List, InputItem, WingBlank, WhiteSpace,Button } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { login } from '../../redux/user.redux.js'
 import { Redirect } from 'react-router-dom'
+import imoocForm from '../../component/imoocForm/imoocForm.js'
 
-
-
-@WrapperHello
-class Hello extends React.Component {
-	render(){
-		return <h2>hello I love react and redux</h2>
-	}
-}
-
-//属性代理
-function WrapperHello(Comp){
-
-	//反向继承
-	class WrapComp extends Comp{
-		componentDidMount(){
-			console.log('高阶组件新增的生命周期')
-		}
-		render(){
-			return <Comp></Comp>
-		}
-	}
-
-
-
-
-	// class WrapComp extends React.Component{
-
-	// 	render(){
-	// 		return(
-	// 			<div>
-	// 				<p>高阶组件</p>
-	// 				<Comp {...this.props}></Comp>
-	// 			</div>
-
-	// 		)
-			
-	// 	}
-	// }
-	return WrapComp
-}
 
 
 
@@ -52,6 +13,7 @@ function WrapperHello(Comp){
 	state => state.user,
 	{ login }
 )
+@imoocForm
 class Login extends React.Component {
 	constructor(props){
 		super(props)
@@ -67,28 +29,21 @@ class Login extends React.Component {
 		this.props.history.push('/register')
 	}
 
-	handleChange(key,val){
-		this.setState({
-			[key]:val
-		})
-	}
-
 	handleLogin(){
-		this.props.login(this.state)
+		this.props.login(this.props.state)
 	}
 
 	render(){
 		return (
 			<div>
-				<Hello/>
 				{this.props.redirectTo && this.props.redirectTo !== '/login' ? <Redirect to={this.props.redirectTo}></Redirect> : null}
 				<Logo></Logo>
 				<WingBlank>
 					<List>
 						{this.props.msg ? <p className="error-Msg">{this.props.msg}</p>:null}
-						<InputItem onChange={(v) => this.handleChange('user',v)}>用户</InputItem>
+						<InputItem onChange={(v) => this.props.handleChange('user',v)}>用户</InputItem>
 						<WhiteSpace/>
-						<InputItem type='password' onChange={(v) => this.handleChange('pwd',v)}>密码</InputItem>
+						<InputItem type='password' onChange={(v) => this.props.handleChange('pwd',v)}>密码</InputItem>
 					</List>
 					<WhiteSpace/>
 					<Button type='primary' onClick={this.handleLogin}>登录</Button>
