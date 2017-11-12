5a8d733d8ff7493c0d1f373d6e0ec778d354975e
GarenWang <2270099149@qq.com>
2017-11-12 16:43:19 +0800

    前后端socket.io联调成功

--- imooc-app/server/server.js
+++ imooc-app/server/server.js
@@ -4,14 +4,22 @@ const userRouter = require('./user')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-
 const app = express()
+
+//work with express
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection',function(socket){
+	console.log('user login')
+})
+
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use('/user',userRouter)
 
-app.listen(9093,() => {
+server.listen(9093,() => {
 	console.log('demo app start at port 9093')
 })
 
